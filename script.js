9b28237e4108786409e63c4b7972ed18faecfa38
jlamond37 <jlamond37@gmail.com>
2022-07-28 16:05:51 -0400

    added formula for colors

--- script.js
+++ script.js
@@ -3,17 +3,37 @@ var hourTimeEl = $('.hour');
 var rowEl = $('.row');
 var hourRows = []
 var hour = moment().format("HH");
+var rightNow;
+var rowsList = document.querySelectorAll(".row");
+// console.log(rowsList);
+console.log(hour)
+rowsList.forEach(function(row){
+  var rowEl = row.children[0];
+  // console.log(rowEl.parentElement);
+  var rowHour = row.children[0].getAttribute("class").split(" ")[1];
+  // console.log(rowHour);
+  if (rowHour < hour) {
+    console.log('inside first if');
+    rowEl.parentElement.setAttribute('style','background-color:grey');
+  } else if (rowHour == hour) {
+    console.log('inside second if');
+    rowEl.parentElement.setAttribute('style','background-color:red');
+  } else {
+    console.log('inside else');
+    rowEl.parentElement.setAttribute('style','background-color:green');
+  }
+})
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY');
+    rightNow = moment().format('MMM DD, YYYY');
     timeDisplayEl.text(rightNow);
   }
 
 function hourTime() {
     var hourlyTime = moment().format('HH');
-    if (rightNow.isAfter(moment('10:00', "HH:mm"))){
-      return rowEl.setAttribute('style','background-color:grey');
-    }
+    // if (rightNow.isAfter(moment('10:00', "HH:mm"))){
+    //   return rowEl.setAttribute('style','background-color:grey');
+    // }
 }
 
 
